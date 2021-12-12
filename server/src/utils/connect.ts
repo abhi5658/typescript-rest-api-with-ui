9659daad1e1eb045c620f6d9c763a55711506459
Abhishek Shah <abhishekshah5658@gmail.com>
2021-12-13 01:08:45 +0530

    add mongo connection

---
+++ server/src/utils/connect.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+async function connect() {
+  const dbUri = config.get<string>('dbUri');
+  try {
+    await mongoose.connect(dbUri);
+    console.log("DB connected");
+  } catch (error) {
+    console.log("Error in connecting DB");
+    process.exit(1);
+  }
+}
+
+export default connect;
